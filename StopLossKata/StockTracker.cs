55cac79a24cbfcecd7518cbfd38098511420784d
Matthew Winder <@mwinder>
2015-10-15 23:36:05 +0100

    init

---
+++ StopLossKata/StockTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StopLossKata
+{
+    class StockTracker
+    {
+        private Broker broker;
+        private bool priceDecreased;
+        private DateTime priceChangedAt;
+        private int initialPrice;
+
+        public StockTracker(Broker broker)
+        {
+            this.broker = broker;
+        }
+
+        public void Buy(int price, DateTime time)
+        {
+            initialPrice = price;
+        }
+
+        public void PriceChanged(int price, DateTime time)
+        {
+            priceDecreased = (price < initialPrice);
+            priceChangedAt = time;
+        }
+
+        public void Refresh(DateTime time)
+        {
+            if (!priceDecreased) return;
+            if ((time - priceChangedAt).Seconds >= 30)
+                broker.Sell();
+        }
+    }
+}
---
+++ StopLossKata/StopLoss.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StopLossKata
+{
+    public class StopLoss : Broker
+    {
+        bool stockSold;
+        StockTracker stockTracker;
+
+        public StopLoss()
+        {
+            stockTracker = new StockTracker(this);
+        }
+
+        public void Sell()
+        {
+            stockSold = true;
+        }
+
+        [Fact]
+        public void StockSoldIfPriceFallsToThresholdForMoreThan30Seconds()
+        {
+            stockTracker.Buy(10, DateTime.Now);
+            stockTracker.PriceChanged(9, DateTime.Now);
+
+            stockTracker.Refresh(DateTime.Now.AddSeconds(30));
+
+            Assert.True(stockSold, "Stock was not sold");
+        }
+
+        [Fact]
+        public void StockNotSoldIfPriceDoesNotFall()
+        {
+            stockTracker.Buy(10, DateTime.Now);
+
+            stockTracker.Refresh(DateTime.Now.AddSeconds(30));
+
+            Assert.False(stockSold, "Stock was sold");
+        }
+
+        [Fact]
+        public void StockNotSoldIfPriceFallsForLessThan30Seconds()
+        {
+            stockTracker.Buy(10, DateTime.Now);
+            stockTracker.PriceChanged(9, DateTime.Now);
+
+            stockTracker.Refresh(DateTime.Now.AddSeconds(25));
+
+            Assert.False(stockSold, "Stock was sold");
+        }
+
+        [Fact]
+        public void StockNotSoldIfIncreasesFor30Seconds()
+        {
+            stockTracker.Buy(10, DateTime.Now);
+            stockTracker.PriceChanged(11, DateTime.Now);
+
+            stockTracker.Refresh(DateTime.Now.AddSeconds(30));
+
+            Assert.False(stockSold, "Stock was sold");
+        }
+    }
+}
